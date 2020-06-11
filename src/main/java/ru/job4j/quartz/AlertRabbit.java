2b0a00eacf411a407803ab9150d25ac4ec8b0cbf
Igor <workuglis@gmail.com>
2020-06-11 16:44:07 +0300

    * 1. Quartz [#279181]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,48 +2,36 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
+import java.util.Properties;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.nio.file.Path;
-
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
  * 1. Quartz [#279181]
  */
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties properties = new Properties();
+            properties.load(in);
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(timeEx())
+                    .withIntervalInSeconds(Integer.parseInt((String) properties.get("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-           se.printStackTrace();
-        }
-    }
-
-    private static int timeEx() {
-        int rsl = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader("rabbit.properties"))) {
-            rsl = Integer.parseInt(reader.readLine().split("=")[1]);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return rsl;
     }
 
     public static class Rabbit implements Job {
