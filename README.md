3d1a2a8275d1ac4c6574c038a032406caa4d9d1e
Igor <workuglis@gmail.com>
2020-06-13 16:29:09 +0300

    2. Парсинг HTML страницы. [#279183]

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Grabber job4j.ru
+
 [![Build Status](https://travis-ci.org/uglis/job4j_grabber.svg?branch=master)](https://travis-ci.org/uglis/job4j_grabber)
 [![codecov](https://codecov.io/gh/uglis/job4j_grabber/branch/master/graph/badge.svg)](https://codecov.io/gh/uglis/job4j_grabber)
---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,61 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2. Парсинг HTML страницы. [#279183]
+ */
+public class SqlRuParse {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+            List<String> nameAndUrl = getNameAndUrl(doc);
+            List<String> dates = getDates(doc);
+            for (int i = 0; i < nameAndUrl.size(); i++) {
+                System.out.println(nameAndUrl.get(i));
+                System.out.println(dates.get(i));
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Получаем список объявлений и их ссылок.
+     *
+     * @param document html file.
+     * @return список.
+     */
+    private static List<String> getNameAndUrl(Document document) {
+        List<String> rsl = new ArrayList<>();
+        Elements elements = document.select(".postslisttopic");
+        for (Element td : elements) {
+            Element href = td.child(0);
+            rsl.add(href.text() + System.lineSeparator() + href.attr("href"));
+        }
+        return rsl;
+    }
+
+    /**
+     * Получаем даты изменения Объявлений.
+     *
+     * @param document html file.
+     * @return список дат.
+     */
+    private static List<String> getDates(Document document) {
+        List<String> rsl = new ArrayList<>();
+        Elements elements = document.select(".altCol");
+        for (int i = 1; i < elements.size(); i++) {
+            rsl.add(elements.get(i).text());
+            i++;
+        }
+        return rsl;
+    }
+}
